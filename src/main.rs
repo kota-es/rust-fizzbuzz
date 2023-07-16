22016937e36dc48a3823f2892da312970036055f
kota-es <info@k-3110.com>
2023-07-16 21:25:43 +0900

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
+fn fizzbuzz(x: i32) -> String {
+    match ((x % 3) == 0, (x % 5) == 0) {
+        (true, true) => "Fizzbuzz".to_string(),
+        (true, _) => "Fizz".to_string(),
+        (_, true) => "Buzz".to_string(),
+        _ => x.to_string(),
+    }
+}
+
 fn main() {
-    (1..=15).for_each(|x| match ((x % 3) == 0, (x % 5) == 0) {
-        (true, true) => println!("Fizzbuzz"),
-        (true, _) => println!("Fizz"),
-        (_, true) => println!("Buzz"),
-        _ => println!("{x}"),
-    })
+    (1..=15).map(fizzbuzz).for_each(|x| println!("{x}"))
+}
+
+#[test]
+fn test() {
+    let test_target: Vec<String> = (1..=15).map(fizzbuzz).collect();
+
+    assert_eq!(
+        test_target,
+        vec![
+            "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "Fizzbuzz"
+        ]
+    );
 }
